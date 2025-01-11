9e23087a4abdbe51dbe68047bee0dba8a2727d72
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2025-01-11 08:49:01 -0300

    DIA-30: makes `npm test` more robust with `minTimeout and removed `&& npm run wait-for-postgres`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,9 +6,11 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      minTimeout: 1000,
     });
 
-    async function fetchStatusPage() {
+    async function fetchStatusPage(_bail, tryNumber) {
+      console.log(`Attempt ${tryNumber} to fetch status page`);
       const response = await fetch("http://localhost:3000/api/v1/status");
       if (response.status !== 200) {
         throw new Error("Status page is not available");
