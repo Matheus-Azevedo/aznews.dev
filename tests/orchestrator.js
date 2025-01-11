0b52e1200c8a756eeb281e803892e3f617665e58
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2025-01-11 09:44:05 -0300

    DIA-30: makes `npm test` more robust with better `error messages`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,4 +1,5 @@
 import retry from "async-retry";
+import status from "statuses";
 
 async function waitForAllServices() {
   await waitForWebServer();
@@ -7,13 +8,19 @@ async function waitForAllServices() {
     return retry(fetchStatusPage, {
       retries: 100,
       minTimeout: 1000,
+      onRetry: (error, attempt) => {
+        console.log(`Attempt ${attempt}: ${error.message}`);
+      },
     });
 
-    async function fetchStatusPage(_bail, tryNumber) {
-      console.log(`Attempt ${tryNumber} to fetch status page`);
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      if (response.status !== 200) {
-        throw new Error("Status page is not available");
+    async function fetchStatusPage() {
+      try {
+        const response = await fetch("http://localhost:3000/api/v1/status");
+        if (response.status !== status("OK")) {
+          throw new Error(`unexpected status code: ${response.status}`);
+        }
+      } catch (error) {
+        throw new Error(`Failed to fetch status page - ${error.message}`);
       }
     }
   }
